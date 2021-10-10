6179b99255cb23b280638a123e2d15b060672b24
Tran Nguyen Thuong Truong <ititiu19228@student.hcmiu.edu.vn>
2021-10-10 10:13:07 +0700

    feat: initial App.vue file

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Main />
+    <Accessibility />
+  </div>
+</template>
+
+<script>
+import Main from "./components/Main.vue";
+import Accessibility from "./components/Accessibility.vue";
+
+export default {
+  name: "App",
+  components: {
+    Main,
+    Accessibility,
+  },
+};
+</script>
