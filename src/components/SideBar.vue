6d8376c7826288a66b901c670f1a7bc67a8b024e
Tran Nguyen Thuong Truong <ititiu19228@student.hcmiu.edu.vn>
2021-10-25 10:39:02 +0700

    add SideBar.vue component

---
+++ src/components/SideBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <section>
+    <a :href="this.author"><img src="../assets/logo.png" /></a>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      author: "https://github.com/thuongtruong1009",
+    };
+  },
+};
+</script>
+
+<style scoped>
+section > a > img {
+  position: absolute;
+  width: 130px;
+  height: 130px;
+  top: 0;
+  right: 0;
+  cursor: pointer;
+  filter: hue-rotate(240deg);
+}
+</style>
