e131ef147883d633a042f297c03b4087bf2f836d
thuongtruong1009 <ititiu19228@student.hcmiu.edu.vn>
2021-12-09 12:46:25 +0700

    fix codefactor

--- src/components/layouts/Followers.vue
+++ src/components/layouts/Followers.vue
@@ -1,16 +1,16 @@
 <template>
-<div class="container">
-      <table class="table table-striped">
-          <tbody>
-              <tr class="table-success" v-for="i in 20" :key="i">
-                  <td>1</td>
-                  <td>oop</td>
-                  <td>new repository</td>
-                  <td>123</td>
-              </tr>
-          </tbody>
-      </table>
-      </div>
+    <div class="container">
+        <table class="table table-striped">
+            <tbody>
+                <tr class="table-success" v-for="i in 20" :key="i">
+                    <td>1</td>
+                    <td>oop</td>
+                    <td>new repository</td>
+                    <td>123</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
@@ -20,7 +20,7 @@ export default {
 </script>
 
 <style scoped>
-table{
+table {
     overflow-y: scroll;
 }
 </style>
