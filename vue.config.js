9cd37150beaa3936421eef71b7ce60ce6645f6bb
thuongtruong1009 <ititiu19228@student.hcmiu.edu.vn>
2021-12-16 00:19:56 +0700

    add table preview infor

--- vue.config.js
+++ vue.config.js
@@ -1,28 +1,6 @@
 const path = require('path');
-// const fs = require('fs');
-// //Find all files in src and make alias
-
-// const dirs = fs.readdirSync(path.resolve(__dirname, 'src'));
-
-// const alias = {
-//     src: path.resolve(__dirname, 'src')
-// }
-
-// dirs.forEach(name => {
-//     const filePath = path.resolve(__dirname, 'src', name);
-//     //Only add folders
-//     if (fs.statSync(filePath).isDirectory()) {
-//         alias[name] = filePath;
-//     }
-
-// });
 module.exports = {
     publicPath: '/searching-github-username/',
-    // configureWebpack: {
-    //     resolve: {
-    //         alias
-    //     },
-    // }
     configureWebpack: {
         resolve: {
             modules: [
