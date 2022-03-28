f368b7305f32dc0c8bd85d2fe0b49b64eb0a71dd
thuongtruong1009 <ititiu19228@student.hcmiu.edu.vn>
2022-03-28 13:19:57 +0700

    handling load more

--- src/components/CRepositories.vue
+++ src/components/CRepositories.vue
@@ -2,7 +2,7 @@
 import { userStore } from '../stores/user';
 import { computed, ref } from 'vue';
 import langColor from '../shared/lang';
-import {alphaSort} from '../shared/sort'
+import { alphaSort } from '../shared/sort'
 import CLoading from '../components/CLoading.vue';
 import IMultiLine from './icons/repos/IMultiLine.vue';
 import ISingleLine from './icons/repos/ISingleLine.vue'
@@ -15,36 +15,43 @@ import IGenerate from './icons/repos/IGenerate.vue'
 const store = userStore()
 
 const filterMode = ref('default')
-const reposComputed = computed(() =>{
-    if(filterMode.value === 'default'){
+const reposComputed = computed(() => {
+    if (filterMode.value === 'default') {
         return alphaSort(store.reposData, "name");
     }
-    else if(filterMode.value === 'most_stars'){
-        return (store.reposData.sort((a:any, b:any) => a.stargazers_count - b.stargazers_count)).reverse();
+    else if (filterMode.value === 'most_stars') {
+        return (store.reposData.sort((a: any, b: any) => a.stargazers_count - b.stargazers_count)).reverse();
     }
-    else if(filterMode.value === 'fewest_star'){
-        return (store.reposData.sort((a:any, b:any) => a.stargazers_count - b.stargazers_count));
+    else if (filterMode.value === 'fewest_star') {
+        return (store.reposData.sort((a: any, b: any) => a.stargazers_count - b.stargazers_count));
     }
-    else if(filterMode.value === 'most_fork'){
-        return (store.reposData.sort((a:any, b:any) => a.forks_count - b.forks_count)).reverse();
+    else if (filterMode.value === 'most_fork') {
+        return (store.reposData.sort((a: any, b: any) => a.forks_count - b.forks_count)).reverse();
     }
-    else if(filterMode.value === 'fewest_fork'){
-        return (store.reposData.sort((a:any, b:any) => a.forks_count - b.forks_count));
+    else if (filterMode.value === 'fewest_fork') {
+        return (store.reposData.sort((a: any, b: any) => a.forks_count - b.forks_count));
     }
-    else if(filterMode.value === 'z_a'){
+    else if (filterMode.value === 'z_a') {
         return alphaSort(store.reposData, "name").reverse();
     }
 })
+const reposVisible = ref(3)
+const step = ref(3)
+const reposVisibleComputed = computed(() => reposComputed.value.slice(0, reposVisible.value))
 </script>
 
 <template>
     <CLoading v-if="store.isLoading === true" />
-    <div class="repositories_view p-5 max-w-238 mx-auto dark:bg-black" v-if="store.isLoading === false">
+    <div
+        class="repositories_view p-5 max-w-238 mx-auto dark:bg-black"
+        v-if="store.isLoading === false"
+    >
         <div class="filter_tab flex justify-between items-center py-5 w-full">
             <select
                 name="filter"
                 id="filter"
-                class="pl-3 py-2 rounded-3xl bg-[#F6F6F6] w-50 max-w-50 text-sm text-[#9595A1] cursor-pointer mr-3" v-model="filterMode"
+                class="pl-3 py-2 rounded-3xl bg-[#F6F6F6] w-50 max-w-50 text-sm text-[#9595A1] cursor-pointer mr-3"
+                v-model="filterMode"
             >
                 <option value="default">Default matches</option>
                 <option value="most_stars">Most stars</option>
@@ -53,31 +60,23 @@ const reposComputed = computed(() =>{
                 <option value="fewest_fork">Fewest forks</option>
                 <option value="z_a">Alphabet Z to A</option>
             </select>
-            <div class="flex items-center gap-5">
-                <div class="flex gap-2 text-[#9595A1]">
-                    <div
-                        class="rounded-lg active:bg-[#F6F6F6] cursor-pointer flex justify-center items-center w-9 h-9"
-                    >
-                        <ISingleLine />
-                    </div>
-                    <div
-                        class="rounded-lg bg-[#F6F6F6] cursor-pointer flex justify-center items-center w-9 h-9"
-                    >
-                        <IMultiLine />
-                    </div>
+            <div class="flex items-center gap-2 text-[#9595A1]">
+                <div
+                    class="rounded-lg active:bg-[#F6F6F6] cursor-pointer flex justify-center items-center w-9 h-9"
+                >
+                    <ISingleLine />
                 </div>
-                <input
-                    type="text"
-                    name="search_repos"
-                    id="search_repos"
-                    class="px-5 rounded-3xl bg-[#F6F6F6] my-0.5 w-70 max-w-70"
-                />
+                <div
+                    class="rounded-lg bg-[#F6F6F6] cursor-pointer flex justify-center items-center w-9 h-9"
+                >
+                    <IMultiLine />
+                </div>
             </div>
         </div>
         <div class="repositories_list w-full">
             <div
                 class="repo flex justify-between items-start bg-[#F6F6F6] border-1 border-solid border-light-700/50 mt-2 rounded-xl py-2 px-5"
-                v-for="repo in reposComputed"
+                v-for="repo in reposVisibleComputed"
                 :key="repo.id"
             >
                 <div class="flex">
@@ -100,7 +99,10 @@ const reposComputed = computed(() =>{
                 <div class="repo_option grid gap-10">
                     <div class="flex text-sm text-[#9595A1] font-medium gap-3">
                         <p class="repo_lang flex items-center gap-1">
-                            <span class="lang-icon w-2 h-2 rounded-full" :style="{background: langColor[repo.language]}"></span>
+                            <span
+                                class="lang-icon w-2 h-2 rounded-full"
+                                :style="{ background: langColor[repo.language] }"
+                            ></span>
                             {{ repo.language }}
                         </p>
                         <p class="repo_star flex items-start gap-0.5">
@@ -112,13 +114,44 @@ const reposComputed = computed(() =>{
                             {{ repo.forks_count }}
                         </p>
                     </div>
-                    <div class="flex justify-end items-end text-sm text-[#9595A1] font-medium gap-3">
-                        <a :href="`${repo.html_url}/archive/HEAD.zip`"><IDownload /></a>
-                        <a :href="repo.forks_url"><IClone /></a>
-                        <a :href="`${repo.html_url}/generate`"><IGenerate /></a>
+                    <div
+                        class="flex justify-end items-end text-sm text-[#9595A1] font-medium gap-3"
+                    >
+                        <a :href="`${repo.html_url}/archive/HEAD.zip`">
+                            <IDownload />
+                        </a>
+                        <a :href="repo.forks_url">
+                            <IClone />
+                        </a>
+                        <a :href="`${repo.html_url}/generate`">
+                            <IGenerate />
+                        </a>
                     </div>
                 </div>
             </div>
         </div>
+        <div class="loadmore-tab">
+            <button class="fill_btn" @click="reposVisible += step"
+        v-if="reposVisible < reposComputed.length">Load more...</button>
+        </div>
     </div>
 </template>
+
+<style scoped>
+.fill_btn {
+    background: none;
+    border: 2px solid;
+    font: inherit;
+    line-height: 1;
+    margin-top: 1em;
+    padding: 0.5em 1.5em;
+    color: #a972cb;
+    transition: 0.25s;
+    border-radius: 0.5em;
+}
+.fill_btn:hover {
+    border-color: #cb72aa;
+    color: #fff;
+    box-shadow: inset 0 0 0 2em #cb72aa;
+}
+</style>
