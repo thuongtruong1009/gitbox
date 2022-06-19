be79edc37a91edb28d543de3efd4cbd0df96607e
thuongtruong1009 <ititiu19228@student.hcmiu.edu.vn>
2022-06-19 13:27:07 +0700

    change item target

--- src/stores/explore.ts
+++ src/stores/explore.ts
@@ -2,6 +2,6 @@ import { defineStore } from 'pinia'
 import { reactive, ref } from 'vue'
 
 export const exploreStore = defineStore('explore', () => {
-    const reposTrending = reactive<any[]>([])
+    const reposTrending = reactive<any>('')
     return { reposTrending }
 })
